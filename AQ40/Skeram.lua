bbe6f420a20c57926e6ac5bc3a1d7e02b04c050b
Maarten Suetens <suetens.maarten@gmail.com>
2015-05-28 23:35:28 +0200

    bigwigs start

---
+++ AQ40/Skeram.lua
@@ -0,0 +1,108 @@
+﻿------------------------------
+--      Are you local?      --
+------------------------------
+
+local boss = AceLibrary("Babble-Boss-2.2")["The Prophet Skeram"]
+local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+local skeramstarted = nil
+----------------------------
+--      Localization      --
+----------------------------
+
+L:RegisterTranslations("enUS", function() return {
+	aetrigger = "The Prophet Skeram begins to cast Arcane Explosion.",
+	mctrigger = "The Prophet Skeram begins to cast True Fulfillment.",
+	splittrigger = "The Prophet Skeram casts Summon Images.",
+	aewarn = "Casting Arcane Explosion!",
+	mcwarn = "Casting Mind Control!",
+	mcplayer = "^([^%s]+) ([^%s]+) afflicted by True Fulfillment.$",
+	mcplayerwarn = "%s is mindcontrolled!",
+	mcbar = "MC: %s",
+	mcyou = "You",
+	mcare = "are",
+
+	win = "The Prophet Skeram has been defeated",
+
+
+	pull1	= "Cower mortals!",
+	pull2	= "Are you so eager to die?",
+	pull3	= "Tremble!",
+	
+	splitwarn = "Splitting!",
+	win = "You only delay... The inevitable...",
+
+	telewarn = "Teleport in 5sec!",
+	bartext = "Teleport",
+
+	cmd = "Skeram",
+	mc_cmd = "mc",
+	mc_name = "Mind Control Alert",
+	mc_desc = "Warn for Mind Control",
+
+	ae_cmd = "ae",
+	ae_name = "Arcane Explosion Alert",
+	ae_desc = "Warn for Arcane Explosion",
+	
+--	split_cmd = "split",
+--	split_name = "Split Alert",
+--	split_desc = "Warn before Create Image",
+} end )
+
+----------------------------------
+--      Module Declaration      --
+----------------------------------
+
+BigWigsSkeram = BigWigs:NewModule(boss)
+BigWigsSkeram.zonename = AceLibrary("Babble-Zone-2.2")["Ahn'Qiraj"]
+BigWigsSkeram.enabletrigger = boss
+BigWigsSkeram.toggleoptions = {"ae", "mc", "bosskill"}
+BigWigsSkeram.revision = tonumber(string.sub("$Revision: 16639 $", 12, -3))
+
+------------------------------
+--      Initialization      --
+------------------------------
+
+function BigWigsSkeram:OnEnable()
+	skeramstarted = nil
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_HOSTILEPLAYER_DAMAGE")
+end
+
+------------------------------
+--      Event Handlers      --
+------------------------------
+
+-- Note that we do not sync the MC at the moment, since you really only care
+-- about people that are MC'ed close to you anyway.
+function BigWigsSkeram:CHAT_MSG_SPELL_PERIODIC_HOSTILEPLAYER_DAMAGE(msg)
+	local _,_, player, type = string.find(msg, L["mcplayer"])
+	if player and type then
+		if player == L["mcyou"] and type == L["mcare"] then
+			player = UnitName("player")
+		end
+		if self.db.profile.mc then
+			self:TriggerEvent("BigWigs_Message", string.format(L["mcplayerwarn"], player), "Important")
+			self:TriggerEvent("BigWigs_StartBar", self, string.format(L["mcbar"], player), 20, "Interface\\Icons\\Spell_Shadow_ShadowWordDominate")
+		end
+	end
+end
+
+
+function BigWigsSkeram:CHAT_MSG_MONSTER_YELL(msg)
+    if string.find(arg1, L["win"]) then 
+        self:TriggerEvent("BigWigs_Message", string.format(AceLibrary("AceLocale-2.2"):new("BigWigs")["%s has been defeated"], self:ToString()), "Bosskill", nil, "Victory")
+	self.core:ToggleModuleActive(self, false)
+          end
+end
+
+
+function BigWigsSkeram:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE(msg)
+	if msg == L["aetrigger"] and self.db.profile.ae then
+		self:TriggerEvent("BigWigs_Message", L["aewarn"], "Urgent")
+	elseif msg == L["mctrigger"] and self.db.profile.mc then
+		self:TriggerEvent("BigWigs_Message", L["mcwarn"], "Urgent")
+	elseif msg == L["splittrigger"] and self.db.profile.split then
+		self:TriggerEvent("BigWigs_Message", L["splitwarn"], "Important")
+	end
+end
---
+++ BWL/Razorgore.lua
@@ -0,0 +1,117 @@
+﻿------------------------------
+--      Are you local?      --
+------------------------------
+
+local boss = AceLibrary("Babble-Boss-2.2")["Razorgore the Untamed"]
+local controller = AceLibrary("Babble-Boss-2.2")["Grethok the Controller"]
+local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+local eggs
+
+----------------------------
+--      Localization      --
+----------------------------
+
+L:RegisterTranslations("enUS", function() return {
+	cmd = "Razorgore",
+
+	start_trigger = "Intruders have breached",
+	start_message = "Razorgore engaged! Mobs in 45sec!Enrage in 15min!",
+	start_incsoon = "Mob Spawn in 20sec!",
+	start_soon = "Mob Spawn in 10sec!",
+	start_verysoon = "Mob Spawn in 5sec!",
+	start_mob = "Mob Spawn",
+        enragebartext = "Enrage",
+	
+	mindcontrol_trigger = "Foolish ([^%s]+).",
+	mindcontrol_message = "%s has been mind controlled!",
+
+	egg_trigger = "casts Destroy Egg",
+	egg_message = "%d/30 eggs destroyed!",
+
+	phase2_trigger = "Razorgore the Untamed's Warming Flames heals Razorgore the Untamed for .*.",
+	phase2_message = "All eggs destroyed, Razorgore loose!",
+
+	mc_cmd = "mindcontrol",
+	mc_name = "Mind Control",
+	mc_desc = "Warn when players are mind controlled",
+
+	eggs_cmd = "eggs",
+	eggs_name = "Don't count eggs",
+	eggs_desc = "Don't count down the remaining eggs - this option does not work for everyone, we need better triggers.",
+
+	phase_cmd = "phase",
+	phase_name = "Phases",
+	phase_desc = "Alert on phase 1 and 2",
+} end)
+
+------------------------------
+--      Module Declaration      --
+----------------------------------
+
+BigWigsRazorgore = BigWigs:NewModule(boss)
+BigWigsRazorgore.zonename = AceLibrary("Babble-Zone-2.2")["Blackwing Lair"]
+BigWigsRazorgore.enabletrigger = { boss, controller }
+BigWigsRazorgore.toggleoptions = { "mc", "eggs", "phase", "bosskill" }
+BigWigsRazorgore.revision = tonumber(string.sub("$Revision: 17555 $", 12, -3))
+
+------------------------------
+--      Initialization      --
+------------------------------
+
+function BigWigsRazorgore:OnEnable()
+	eggs = 0
+
+	self:RegisterEvent("CHAT_MSG_PLAYER_YELL")
+	self:RegisterEvent("CHAT_MSG_SPELL_FRIENDLYPLAYER_BUFF")
+	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+
+	self:RegisterEvent("BigWigs_RecvSync")
+	self:TriggerEvent("BigWigs_ThrottleSync", "RazorgoreEgg", 8)
+end
+
+------------------------------
+--      Event Handlers      --
+------------------------------
+
+function BigWigsRazorgore:CHAT_MSG_PLAYER_YELL(msg)
+	if string.find(msg, L["start_trigger"]) then
+		if self.db.profile.phase then
+		        self:TriggerEvent("BigWigs_StartBar", self, L["enragebartext"], 900, "Interface\\Icons\\Spell_Shadow_UnholyFrenzy")
+			self:TriggerEvent("BigWigs_Message", L["start_message"], "Urgent")
+			self:TriggerEvent("BigWigs_StartBar", self, L["start_mob"], 45, "Interface\\Icons\\Spell_Holy_PrayerOfHealing")
+			self:ScheduleEvent("BigWigs_Message", 25, L["start_incsoon"], "Important")
+			self:ScheduleEvent("BigWigs_Message", 35, L["start_soon"], "Important")
+			self:ScheduleEvent("BigWigs_Message", 40, L["start_verysoon"], "Important")
+		end
+		eggs = 0
+	elseif self.db.profile.mc then
+		local _, _, player = string.find(msg, L["mindcontrol_trigger"]);
+		if player then
+			self:TriggerEvent("BigWigs_Message", string.format(L["mindcontrol_message"], player), "Important")
+		end
+	end
+end
+
+function BigWigsRazorgore:CHAT_MSG_SPELL_FRIENDLYPLAYER_BUFF(msg)
+	if string.find(msg, L["egg_trigger"]) then
+		self:TriggerEvent("BigWigs_SendSync", "RazorgoreEgg "..tostring(eggs + 1))
+	end
+end
+
+function BigWigsRazorgore:BigWigs_RecvSync(sync, rest)
+	if sync ~= "RazorgoreEgg" or not rest then return end
+	rest = tonumber(rest)
+
+	if rest == (eggs + 1) then
+		eggs = eggs + 1
+		if not self.db.profile.eggs then
+			self:TriggerEvent("BigWigs_Message", string.format(L["egg_message"], eggs), "Positive")
+		end
+
+		if eggs == 30 and self.db.profile.phase then
+			self:TriggerEvent("BigWigs_Message", L["phase2_message"], "Important")
+		end
+	end
+end
+
+
